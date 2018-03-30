5a7fe28f880020b69b6aadda10dc1051b048f754
Michael Wirth <michael.wirth@mimacom.com>
2018-03-30 23:07:34 +0200

    Set default profile

--- infrastructure-messagebroker-ws-avam-sink/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamWebServiceSinkApplication.java
+++ infrastructure-messagebroker-ws-avam-sink/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamWebServiceSinkApplication.java
@@ -1,15 +1,25 @@
 package ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.avam;
 
+import static org.springframework.core.env.AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME;
+
+import com.google.common.collect.ImmutableMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import ch.admin.seco.jobs.services.jobadservice.application.ProfileRegistry;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableConfigurationProperties(AvamProperties.class)
 public class AvamWebServiceSinkApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AvamWebServiceSinkApplication.class, args);
+        SpringApplication app = new SpringApplication(AvamWebServiceSinkApplication.class);
+        app.setDefaultProperties(ImmutableMap.of(DEFAULT_PROFILES_PROPERTY_NAME, ProfileRegistry.AVAM_WSDL_V1));
+        app.run(args);
     }
 }
