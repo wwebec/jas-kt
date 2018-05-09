2edaec3928fc59ac79249e45f80ca786792fb823
Julien Studzinski <julien.studzinski@mimacomc.om>
2018-05-09 17:44:58 +0200

    JR2-938 fixed

--- infrastructure-messagebroker-shared/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamDateTimeFormatter.java
+++ infrastructure-messagebroker-shared/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamDateTimeFormatter.java
@@ -1,21 +1,30 @@
 package ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.avam;
 
-import static org.springframework.util.StringUtils.hasText;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
+import static org.springframework.util.StringUtils.hasText;
 
 public class AvamDateTimeFormatter {
 
-    private static final DateTimeFormatter AVAM_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss.SSSSSS");
+    private static final DateTimeFormatter FORMAT_AVAM_DATE_FORMATTER = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd")
+            .toFormatter();
+
+    private static final DateTimeFormatter PARSE_AVAM_DATE_FORMATTER = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd-HH.mm.ss")
+            .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
+            .toFormatter();
 
     public static String formatLocalDate(LocalDate localDate) {
-        return (localDate != null) ? localDate.format(AVAM_DATE_FORMATTER) : null;
+        return (localDate != null) ? localDate.format(FORMAT_AVAM_DATE_FORMATTER) + "-00.00.00.0" : null;
     }
 
     public static LocalDate parseToLocalDate(String date) {
         if (hasText(date)) {
-            return LocalDate.parse(date, AVAM_DATE_FORMATTER);
+            return LocalDate.parse(date, PARSE_AVAM_DATE_FORMATTER);
         }
         return null;
     }
