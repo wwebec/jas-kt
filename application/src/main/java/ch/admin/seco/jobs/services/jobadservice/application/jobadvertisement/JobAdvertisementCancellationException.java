5a7a06f3e50881345671af6378579f1139fc59b5
Julien Studzinski <julien.studzinski@mimacomc.om>
2018-06-22 16:02:54 +0200

    refactored backend for cancellation with accessToken

--- application/src/main/java/ch/admin/seco/jobs/services/jobadservice/application/jobadvertisement/JobAdvertisementCancellationException.java
+++
@@ -1,7 +0,0 @@
-package ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement;
-
-public class JobAdvertisementCancellationException extends RuntimeException {
-	public JobAdvertisementCancellationException(String message) {
-		super(message);
-	}
-}
