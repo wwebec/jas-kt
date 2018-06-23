346f34325a2eb90152f5e17487bc750a6afd75e3
Julien Studzinski <julien.studzinski@mimacomc.om>
2018-06-23 16:44:43 +0200

    fixed more secured accessToken

--- domain/src/main/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/AccessTokenGenerator.java
+++ domain/src/main/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/AccessTokenGenerator.java
@@ -1,28 +1,31 @@
 package ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement;
 
-import ch.admin.seco.jobs.services.jobadservice.core.time.TimeMachine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Base64;
 
 @Component
 public class AccessTokenGenerator {
 
+    public static final int RANDOM_NUMBER_LENGTH = 64;
     private static Logger LOG = LoggerFactory.getLogger(AccessTokenGenerator.class);
 
     public String generateToken() {
-        String stamp = TimeMachine.now().toString();
-        return toBase64(md5Of(stamp));
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[RANDOM_NUMBER_LENGTH];
+        random.nextBytes(bytes);
+        return toBase64(md5Of(bytes));
     }
 
-    private byte[] md5Of(String value) {
+    private byte[] md5Of(byte[] bytes) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(value.getBytes());
+            md.update(bytes);
             return md.digest();
         } catch (NoSuchAlgorithmException e) {
             LOG.error("MD5 algorithm not found", e);
