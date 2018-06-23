18eaf658433f0f17e139f0239e9dc69be792c199
Julien Studzinski <julien.studzinski@mimacomc.om>
2018-06-23 17:02:01 +0200

    fixed test of more secured accessToken

--- domain/src/test/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/AccessTokenGeneratorTest.java
+++ domain/src/test/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/AccessTokenGeneratorTest.java
@@ -1,9 +1,6 @@
 package ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement;
 
-import ch.admin.seco.jobs.services.jobadservice.core.time.TimeMachine;
 import org.junit.Test;
-
-import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,13 +8,13 @@ public class AccessTokenGeneratorTest {
 
     @Test
     public void testGenerateToken() {
-        TimeMachine.useFixedClockAt(LocalDateTime.of(2018, 1, 1, 1, 2, 3, 4));
         AccessTokenGenerator generator = new AccessTokenGenerator();
 
         String token = generator.generateToken();
 
-        assertThat(token).isNotNull();
-        assertThat(token).isEqualTo("tq0khqnjnFjyTEmPGnxgQA==");
+        assertThat(token)
+                .isNotNull()
+                .hasSize(24);
     }
 
 }
