e36be94ea05de7f4bd9377328ae52076bca59fa0
Julien Studzinski <julien.studzinski@mimacomc.om>
2018-06-28 10:30:49 +0200

    JR2-1116 Also API jobs should NOT generate a cancellation email. Only those API jobs that are validated by the RAV get a cancellation email.

--- application/src/main/java/ch/admin/seco/jobs/services/jobadservice/application/jobadvertisement/JobAdvertisementMailEventListener.java
+++ application/src/main/java/ch/admin/seco/jobs/services/jobadservice/application/jobadvertisement/JobAdvertisementMailEventListener.java
@@ -138,8 +138,11 @@ public class JobAdvertisementMailEventListener {
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     void onCancelled(JobAdvertisementCancelledEvent event) {
-        LOG.debug("EVENT catched for mail: JOB_ADVERTISEMENT_CANCELLED for JobAdvertisementId: '{}'", event.getAggregateId().getValue());
         final JobAdvertisement jobAdvertisement = getJobAdvertisement(event.getAggregateId());
+        if (jobAdvertisement.getSourceSystem().equals(SourceSystem.API) && (jobAdvertisement.getStellennummerAvam() == null)) {
+            return;
+        }
+        LOG.debug("EVENT catched for mail: JOB_ADVERTISEMENT_CANCELLED for JobAdvertisementId: '{}'", event.getAggregateId().getValue());
         Locale contactLocale = new Locale(DEFAULT_LANGUAGE);
         if(jobAdvertisement.getContact() != null) {
             contactLocale = jobAdvertisement.getContact().getLanguage();
