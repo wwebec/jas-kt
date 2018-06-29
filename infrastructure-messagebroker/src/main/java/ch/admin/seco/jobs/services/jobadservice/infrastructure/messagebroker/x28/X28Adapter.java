8e921ad3169a00f6061c2c0f9b2ac437276fb0c5
Julien Studzinski <julien.studzinski@mimacomc.om>
2018-06-30 00:25:05 +0200

    improve transaction

--- infrastructure-messagebroker/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/x28/X28Adapter.java
+++ infrastructure-messagebroker/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/x28/X28Adapter.java
@@ -1,23 +1,22 @@
 package ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.x28;
 
-import static ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.MessageBrokerChannels.CREATE_FROM_X28_CONDITION;
-import static ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.MessageBrokerChannels.JOB_AD_ACTION_CHANNEL;
-
+import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementAlreadyExistsException;
+import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationService;
 import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.create.CreateJobAdvertisementFromX28Dto;
+import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.update.UpdateJobAdvertisementFromX28Dto;
 import ch.admin.seco.jobs.services.jobadservice.domain.jobadvertisement.JobAdvertisementRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementAlreadyExistsException;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.JobAdvertisementApplicationService;
-import ch.admin.seco.jobs.services.jobadservice.application.jobadvertisement.dto.update.UpdateJobAdvertisementFromX28Dto;
-import org.springframework.transaction.annotation.Transactional;
+import static ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.MessageBrokerChannels.CREATE_FROM_X28_CONDITION;
+import static ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.MessageBrokerChannels.JOB_AD_ACTION_CHANNEL;
 
 @Service
-@Transactional
 public class X28Adapter {
 
     private static final Logger LOG = LoggerFactory.getLogger(X28Adapter.class);
@@ -26,27 +25,38 @@ public class X28Adapter {
 
     private final JobAdvertisementRepository jobAdvertisementRepository;
 
-    public X28Adapter(JobAdvertisementApplicationService jobAdvertisementApplicationService, JobAdvertisementRepository jobAdvertisementRepository) {
+    private final TransactionTemplate transactionTemplate;
+
+    public X28Adapter(JobAdvertisementApplicationService jobAdvertisementApplicationService, JobAdvertisementRepository jobAdvertisementRepository, TransactionTemplate transactionTemplate) {
         this.jobAdvertisementApplicationService = jobAdvertisementApplicationService;
         this.jobAdvertisementRepository = jobAdvertisementRepository;
+        this.transactionTemplate = transactionTemplate;
     }
 
     @StreamListener(target = JOB_AD_ACTION_CHANNEL, condition = CREATE_FROM_X28_CONDITION)
     public void handleCreateFromX28Action(CreateJobAdvertisementFromX28Dto createFromX28) {
-        try {
-            if((createFromX28.getStellennummerEgov() == null) || !exists(createFromX28.getStellennummerEgov())) {
-                jobAdvertisementApplicationService.createFromX28(createFromX28);
-            } else {
-                UpdateJobAdvertisementFromX28Dto updateFromX28 = new UpdateJobAdvertisementFromX28Dto(
-                        createFromX28.getStellennummerEgov(),
-                        createFromX28.getFingerprint(),
-                        createFromX28.getProfessionCodes()
-                );
-                jobAdvertisementApplicationService.updateFromX28(updateFromX28);
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                try {
+                    if (createFromX28.getStellennummerEgov() != null && exists(createFromX28.getStellennummerEgov())) {
+                        jobAdvertisementApplicationService.updateFromX28(prepareUpdateJobAdvertisementFromX28Dto(createFromX28));
+                    } else {
+                        jobAdvertisementApplicationService.createFromX28(createFromX28);
+                    }
+                } catch (JobAdvertisementAlreadyExistsException e) {
+                    LOG.debug(e.getMessage());
+                }
             }
-        } catch (JobAdvertisementAlreadyExistsException e) {
-            LOG.debug(e.getMessage());
-        }
+        });
+    }
+
+    private UpdateJobAdvertisementFromX28Dto prepareUpdateJobAdvertisementFromX28Dto(CreateJobAdvertisementFromX28Dto createFromX28) {
+        return new UpdateJobAdvertisementFromX28Dto(
+                createFromX28.getStellennummerEgov(),
+                createFromX28.getFingerprint(),
+                createFromX28.getProfessionCodes()
+        );
     }
 
     private boolean exists(String stellennummerEgov) {
