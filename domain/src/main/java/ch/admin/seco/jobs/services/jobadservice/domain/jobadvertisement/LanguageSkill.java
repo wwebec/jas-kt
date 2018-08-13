34b264e3f08b22ad2edc9e1502a0602219f94cc9
Patrick Dobler <patrick.dobler@gmail.com>
2018-08-13 14:55:11 +0200

    JR2-1228: cleanup

--- domain/src/main/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/LanguageSkill.java
+++ domain/src/main/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/LanguageSkill.java
@@ -14,87 +14,87 @@ import java.util.Objects;
 @Access(AccessType.FIELD)
 public class LanguageSkill implements ValueObject<LanguageSkill> {
 
-	@LanguageIsoCode
-	@NotBlank
-	private String languageIsoCode;
+    @LanguageIsoCode
+    @NotBlank
+    private String languageIsoCode;
 
-	@Enumerated(EnumType.STRING)
-	private LanguageLevel spokenLevel;
+    @Enumerated(EnumType.STRING)
+    private LanguageLevel spokenLevel;
 
-	@Enumerated(EnumType.STRING)
-	private LanguageLevel writtenLevel;
+    @Enumerated(EnumType.STRING)
+    private LanguageLevel writtenLevel;
 
-	protected LanguageSkill() {
-		// For reflection libs
-	}
+    protected LanguageSkill() {
+        // For reflection libs
+    }
 
-	public LanguageSkill(Builder builder) {
-		this.languageIsoCode = Condition.notBlank(builder.languageIsoCode).toLowerCase();
-		this.spokenLevel = builder.spokenLevel;
-		this.writtenLevel = builder.writtenLevel;
-	}
+    public LanguageSkill(Builder builder) {
+        this.languageIsoCode = Condition.notBlank(builder.languageIsoCode).toLowerCase();
+        this.spokenLevel = builder.spokenLevel;
+        this.writtenLevel = builder.writtenLevel;
+    }
 
-	public String getLanguageIsoCode() {
-		return languageIsoCode;
-	}
+    public String getLanguageIsoCode() {
+        return languageIsoCode;
+    }
 
-	public LanguageLevel getSpokenLevel() {
-		return spokenLevel;
-	}
+    public LanguageLevel getSpokenLevel() {
+        return spokenLevel;
+    }
 
-	public LanguageLevel getWrittenLevel() {
-		return writtenLevel;
-	}
+    public LanguageLevel getWrittenLevel() {
+        return writtenLevel;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) { return true; }
-		if (o == null || getClass() != o.getClass()) { return false; }
-		LanguageSkill that = (LanguageSkill) o;
-		return Objects.equals(languageIsoCode, that.languageIsoCode) &&
-				spokenLevel == that.spokenLevel &&
-				writtenLevel == that.writtenLevel;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
+        LanguageSkill that = (LanguageSkill) o;
+        return Objects.equals(languageIsoCode, that.languageIsoCode) &&
+                spokenLevel == that.spokenLevel &&
+                writtenLevel == that.writtenLevel;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(languageIsoCode, spokenLevel, writtenLevel);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(languageIsoCode, spokenLevel, writtenLevel);
+    }
 
-	@Override
-	public String toString() {
-		return "LanguageSkill{" +
-				"languageIsoCode='" + languageIsoCode + '\'' +
-				", spokenLevel=" + spokenLevel +
-				", writtenLevel=" + writtenLevel +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "LanguageSkill{" +
+                "languageIsoCode='" + languageIsoCode + '\'' +
+                ", spokenLevel=" + spokenLevel +
+                ", writtenLevel=" + writtenLevel +
+                '}';
+    }
 
-	public static final class Builder {
-		private String languageIsoCode;
-		private LanguageLevel spokenLevel;
-		private LanguageLevel writtenLevel;
+    public static final class Builder {
+        private String languageIsoCode;
+        private LanguageLevel spokenLevel;
+        private LanguageLevel writtenLevel;
 
-		public Builder() {
-		}
+        public Builder() {
+        }
 
-		public LanguageSkill build() {
-			return new LanguageSkill(this);
-		}
+        public LanguageSkill build() {
+            return new LanguageSkill(this);
+        }
 
-		public Builder setLanguageIsoCode(String languageIsoCode) {
-			this.languageIsoCode = languageIsoCode;
-			return this;
-		}
+        public Builder setLanguageIsoCode(String languageIsoCode) {
+            this.languageIsoCode = languageIsoCode;
+            return this;
+        }
 
-		public Builder setSpokenLevel(LanguageLevel spokenLevel) {
-			this.spokenLevel = spokenLevel;
-			return this;
-		}
+        public Builder setSpokenLevel(LanguageLevel spokenLevel) {
+            this.spokenLevel = spokenLevel;
+            return this;
+        }
 
-		public Builder setWrittenLevel(LanguageLevel writtenLevel) {
-			this.writtenLevel = writtenLevel;
-			return this;
-		}
-	}
+        public Builder setWrittenLevel(LanguageLevel writtenLevel) {
+            this.writtenLevel = writtenLevel;
+            return this;
+        }
+    }
 }
