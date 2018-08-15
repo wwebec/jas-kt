66578529ac5b52671655b53ca890ec1df9820524
Valerii Rusakov <valerii.rusakov@mimacom.com>
2018-08-15 16:50:30 +0300

    JR2-1073 Fix wrong logic in unit test

--- application/src/test/java/ch/admin/seco/jobs/services/jobadservice/application/jobadvertisement/JobAdvertisementApplicationServiceTest.java
+++ application/src/test/java/ch/admin/seco/jobs/services/jobadservice/application/jobadvertisement/JobAdvertisementApplicationServiceTest.java
@@ -126,7 +126,7 @@ public class JobAdvertisementApplicationServiceTest {
                 Collections.singletonList(new JobDescriptionDto("de", "title", "description")),
                 new CompanyDto("name", "street", "houseNumber", "postalCode", "city", "CH", null, null, null, "phone", "email", "website", false),
                 null,
-                new EmploymentDto(LocalDate.of(2018, 1, 1), LocalDate.of(2018, 1, 10), false, false, false, 80, 100, null),
+                new EmploymentDto(LocalDate.of(2018, 1, 1), LocalDate.of(2018, 12, 31), false, false, false, 80, 100, null),
                 new CreateLocationDto("remarks", "city", "postalCode", "CH"),
                 new OccupationDto("avamCode", WorkExperience.MORE_THAN_1_YEAR, "educationCode"),
                 Collections.singletonList(new LanguageSkillDto("de", LanguageLevel.PROFICIENT, LanguageLevel.PROFICIENT)),
@@ -139,7 +139,8 @@ public class JobAdvertisementApplicationServiceTest {
     public void shouldSetReportingObligationToFalseWhenShortEmployment() {
         //Prepare
         CreateJobAdvertisementDto createJobAdvertisementDto = createDefaultJobAdvertisementDto();
-        createJobAdvertisementDto.setEmployment(new EmploymentDto(LocalDate.of(2018, 1, 1), LocalDate.of(2018, 1, 10), false, false, false, 80, 100, null));
+        createJobAdvertisementDto.setEmployment(new EmploymentDto(LocalDate.of(2018, 1, 1), LocalDate.of(2018, 1, 10), true, false, false, 80, 100, null));
+        when(reportingObligationService.hasReportingObligation(any(), any(), any())).thenReturn(true);
 
         checkReportingObligation(createJobAdvertisementDto, false);
     }
