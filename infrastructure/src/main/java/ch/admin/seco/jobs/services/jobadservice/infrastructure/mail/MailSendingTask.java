cf504208aff357c537085c464f98d339ea50f8f8
Patrick Dobler <patrick.dobler@gmail.com>
2018-08-21 14:05:31 +0200

    JR2-1273: cleanup added toString method

--- infrastructure/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/mail/MailSendingTask.java
+++ infrastructure/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/mail/MailSendingTask.java
@@ -53,6 +53,13 @@ class MailSendingTask {
     }
 
     @Override
+    public String toString() {
+        return "MailSendingTask{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) { return true; }
         if (o == null || getClass() != o.getClass()) { return false; }
