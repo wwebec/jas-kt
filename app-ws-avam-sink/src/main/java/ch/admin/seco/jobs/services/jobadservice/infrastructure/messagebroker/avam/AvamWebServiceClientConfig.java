7bd3c94743e56e6cdf07ff5383548640ce7e22f2
Marton Biro <marton.biro@mimacom.com>
2018-09-28 17:12:02 +0200

    Use utf-8 encoding for the avam WS

--- app-ws-avam-sink/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamWebServiceClientConfig.java
+++ app-ws-avam-sink/src/main/java/ch/admin/seco/jobs/services/jobadservice/infrastructure/messagebroker/avam/AvamWebServiceClientConfig.java
@@ -2,20 +2,29 @@ package ch.admin.seco.jobs.services.jobadservice.infrastructure.messagebroker.av
 
 import ch.admin.seco.jobs.services.jobadservice.infrastructure.ws.avam.DeliverOste;
 import ch.admin.seco.jobs.services.jobadservice.infrastructure.ws.avam.DeliverOsteResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.WebServiceClientException;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
+import org.springframework.ws.client.support.interceptor.ClientInterceptorAdapter;
+import org.springframework.ws.context.MessageContext;
+import org.springframework.ws.transport.HeadersAwareSenderWebServiceConnection;
+import org.springframework.ws.transport.WebServiceConnection;
+import org.springframework.ws.transport.context.TransportContext;
+import org.springframework.ws.transport.context.TransportContextHolder;
 
-import java.util.Collections;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static javax.xml.bind.Marshaller.JAXB_ENCODING;
+import java.io.IOException;
 
 @Configuration
 @EnableConfigurationProperties(AvamProperties.class)
 public class AvamWebServiceClientConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(AvamWebServiceClientConfig.class);
+
     private final AvamProperties avamProperties;
 
     public AvamWebServiceClientConfig(AvamProperties avamProperties) {
@@ -28,6 +37,23 @@ public class AvamWebServiceClientConfig {
         webServiceTemplate.setDefaultUri(avamProperties.getEndPointUrl());
         webServiceTemplate.setMarshaller(marshaller());
         webServiceTemplate.setUnmarshaller(marshaller());
+        webServiceTemplate.setInterceptors(new ClientInterceptor[]{new ClientInterceptorAdapter() {
+            @Override
+            public boolean handleRequest(MessageContext messageContext) throws WebServiceClientException {
+                TransportContext context = TransportContextHolder.getTransportContext();
+                WebServiceConnection connection = context.getConnection();
+                if (connection instanceof HeadersAwareSenderWebServiceConnection) {
+                    try {
+                        ((HeadersAwareSenderWebServiceConnection) connection).addRequestHeader("Content-Type", "text/xml;charset=UTF-8");
+                    } catch (IOException e) {
+                        LOG.error("Failed to add Content-Type header", e);
+                    }
+                }
+
+                return true;
+            }
+        }});
+
         return webServiceTemplate;
     }
 
@@ -43,7 +69,6 @@ public class AvamWebServiceClientConfig {
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         marshaller.setClassesToBeBound(DeliverOste.class, DeliverOsteResponse.class);
-        marshaller.setMarshallerProperties(Collections.singletonMap(JAXB_ENCODING, UTF_8.name()));
 
         return marshaller;
     }
