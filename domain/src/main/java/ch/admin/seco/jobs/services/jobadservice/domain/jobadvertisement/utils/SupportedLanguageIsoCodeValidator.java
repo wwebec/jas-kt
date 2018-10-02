b1f794a22d849825f854a4f8b1e7166de1e62c50
Marton Biro <marton.biro@mimacom.com>
2018-10-02 16:26:30 +0200

    Add comment

--- domain/src/main/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/utils/SupportedLanguageIsoCodeValidator.java
+++ domain/src/main/java/ch/admin/seco/jobs/services/jobadservice/domain/jobadvertisement/utils/SupportedLanguageIsoCodeValidator.java
@@ -55,8 +55,8 @@ public class SupportedLanguageIsoCodeValidator implements ConstraintValidator<Su
 		SUPPORTED_LANGUAGES.add("fa");
 		SUPPORTED_LANGUAGES.add("prs");
 		SUPPORTED_LANGUAGES.add("ti");
-		SUPPORTED_LANGUAGES.add("98");
-		SUPPORTED_LANGUAGES.add("99");
+		SUPPORTED_LANGUAGES.add("98"); //other
+		SUPPORTED_LANGUAGES.add("99"); //not specified
 	}
 
 	@Override
